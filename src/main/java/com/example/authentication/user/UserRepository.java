e1db3733f1010195a85f9e998d424e97e8c0758b
Rafael Barreto <rafaelgavabarreto@gmail.com>
2023-09-02 18:40:28 -0400

    fix for the first run

--- src/main/java/com/example/authentication/user/UserRepository.java
+++ src/main/java/com/example/authentication/user/UserRepository.java
@@ -1,12 +1,14 @@
 package com.example.authentication.user;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
+import java.util.UUID;
 
 @Repository
 @Transactional(readOnly = true)
-public interface UserRepository {
+public interface UserRepository extends JpaRepository<User, UUID> {
     Optional<User> findByEmail(String email);
 }
