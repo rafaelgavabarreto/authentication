f3ccf16827b8f11a9e2ee1799d2526d8540f8e79
Rafael Barreto <rafaelgavabarreto@gmail.com>
2023-09-04 22:34:21 -0400

    moving registration to authentication

--- src/main/java/com/example/authentication/registration/RegistrationRequest.java
+++ src/main/java/com/example/authentication/registration/RegistrationRequest.java
@@ -1,17 +1,17 @@
-package com.example.authentication.registration;
-
-import lombok.AllArgsConstructor;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.ToString;
-
-@Getter
-@AllArgsConstructor
-@EqualsAndHashCode
-@ToString
-public class RegistrationRequest {
-    private final String firstName;
-    private final String lastName;
-    private final String password;
-    private final String email;
-}
+//package com.example.authentication.registration;
+//
+//import lombok.AllArgsConstructor;
+//import lombok.EqualsAndHashCode;
+//import lombok.Getter;
+//import lombok.ToString;
+//
+//@Getter
+//@AllArgsConstructor
+//@EqualsAndHashCode
+//@ToString
+//public class RegistrationRequest {
+//    private final String firstName;
+//    private final String lastName;
+//    private final String password;
+//    private final String email;
+//}
--- src/main/java/com/example/authentication/registration/RegistrationService.java
+++ src/main/java/com/example/authentication/registration/RegistrationService.java
@@ -1,10 +1,31 @@
-package com.example.authentication.registration;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class RegistrationService {
-    public String register(RegistrationRequest request) {
-        return "works";
-    }
-}
+//package com.example.authentication.registration;
+//
+//import com.example.authentication.user.User;
+//import com.example.authentication.user.UserRole;
+//import com.example.authentication.user.UserService;
+//import lombok.AllArgsConstructor;
+//import org.springframework.stereotype.Service;
+//
+//@Service
+//@AllArgsConstructor
+//public class RegistrationService {
+//
+//    private final EmailValidator emailValidator;
+//    private final UserService userService;
+//
+//    public String register(RegistrationRequest request) {
+//        boolean isValidEmail = emailValidator.test(request.getEmail());
+//        if(!isValidEmail) {
+//            throw new IllegalStateException("Email not valid");
+//        }
+//        return userService.createUser(new User(
+//                request.getFirstName(),
+//                request.getLastName(),
+//                request.getEmail(),
+//                request.getPassword(),
+//                UserRole.USER,
+//                true,
+//                false
+//        ));
+//    }
+//}
