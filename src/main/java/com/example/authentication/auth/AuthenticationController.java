38850ab619d7a33caa9079a01d8b9b053d7d4269
Rafael Barreto <rafaelgavabarreto@gmail.com>
2023-09-09 20:05:05 -0400

    updating

--- src/main/java/com/example/authentication/auth/AuthenticationController.java
+++ src/main/java/com/example/authentication/auth/AuthenticationController.java
@@ -15,14 +15,14 @@ public class AuthenticationController {
     private final AuthenticationService authenticationService;
 
     @PostMapping("/register")
-    public ResponseEntity<AuthenticationResponse> register (
+    public ResponseEntity<AuthenticationResponse> register(
             @RequestBody RegisterRequest request
     ) {
         return ResponseEntity.ok(authenticationService.register(request));
     }
 
     @PostMapping("/authentication")
-    public ResponseEntity<AuthenticationResponse> authenticate (
+    public ResponseEntity<AuthenticationResponse> authenticate(
             @RequestBody AuthenticationRequest request
     ) {
         return ResponseEntity.ok(authenticationService.authenticate(request));
